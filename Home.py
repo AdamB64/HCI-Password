0208db9a12836a98137065f9e08b5f1a65ff6e4f
Adam <adam2004buchan@outlook.com>
2022-12-07 08:52:00 +0000

    newest version

--- Home.py
+++ Home.py
@@ -1,12 +1,8 @@
-from guizero import App,PushButton,TextBox,Window,Text
+from guizero import App,PushButton,TextBox,Window,Text,ListBox
 from datetime import datetime
 import random 
 
 app = App(title="Passwords")
-
-d = datetime.now()
-
-l=[]
 
 def Password():
     app.show()
@@ -30,40 +26,41 @@ def saved():
     app3.show()
     app4.hide()
 
-def descriptim():
-    i = input("What is the descrition you want for the password")
+app4 =Window(app,title="Desription",height=150)
+
+def description():
+    while i.value =="":
+        t2.show()
+        return i
+    t2.hide()
+    app4.hide()
     return i
 
-app4 =Window(app,title="Desription",height=75)
 
 def save():
     app4.show()
-    global l
+    while len(input.value) >8:
+        input.clear()
+        input.append("Only 8 numbers")
+        return input.value
+    app4.show()
     h=[]
-    o=[input.value,d,i.value]
+    o=[input.value,datetime.now(),i.value]
     h.append(o)
     app.hide()
     i.clear()
-    l=h
-    
+    for n in h:
+        u.append(n)
+      
 
 def generate():
-    p1=random.randint(1,10)
-    p2=random.randint(1,10)
-    p3=random.randint(1,10)
-    p4=random.randint(1,10)
-    p5=random.randint(1,10)
-    p6=random.randint(1,10)
-    p7=random.randint(1,10)
-    p8=random.randint(1,10)
-    p=[p1,p2,p3,p4,p5,p6,p7,p8]
+    p= random.randint(11111111,99999999)
     input.value=p
 
 app6=Window(app)
 
 def toString():
-    for u in range(0,14):
-        u.value=i[u]
+    u.show()
 
 def exit():
     app.destroy()
@@ -73,21 +70,21 @@ def exit():
 exits = PushButton(app,text="Exit",align="top",command=exit)
 exits.text_color=255, 45, 0
 
-time =Text(app,text=d)
+time =Text(app,text=datetime.now())
 
 button1 = PushButton(app,text="To See Saved Passwords",command=saved)
 
 button2 = PushButton(app,text="Click here to generate new password",command=Generator)
 
 #buttons for generator screen
-time =Text(app2,text=d)
+time =Text(app2,text=datetime.now())
 
 exits = PushButton(app2,text="Exit",align="top",command=exit)
 exits.text_color=255, 45, 0
 
 homes = PushButton(app2,text="Home",command=Password)
 
-input = TextBox(app2,text="Type in password (Number only)",width=50)
+input = TextBox(app2,text="Type in password (Number only and only 8 numbers)",width=50)
 
 button3 = PushButton(app2,text="save Password",command=save)
 
@@ -96,7 +93,7 @@ button4 = PushButton(app2,text="Find your Password here",command=saved)
 button5 = PushButton(app2,text="Generate Password",command=generate)
 
 #saves passwords screen buttons
-time =Text(app3,text=d)
+time =Text(app3,text=datetime.now())
 
 exits = PushButton(app3,text="Exit",align="top",command=exit)
 exits.text_color=255, 45, 0
@@ -105,14 +102,18 @@ homes = PushButton(app3,text="Home",command=Password)
 
 button6 = PushButton(app3,text="Click here to go to pass generator",command=Generator)
 
-button7 = PushButton(app3,text="Display last passwords",command=toString)
+button7 = PushButton(app3,text="Display all passwords",command=toString)
 
-time =Text(app4,text=d)
+time =Text(app4,text=datetime.now())
 
 t = Text(app4,text="Type your description below")
 i= TextBox(app4,width=50)
+t2= Text(app4,text="password needs a secription")
+t2.hide()
 
-u =Text(app3)
+button8 =PushButton(app4,text="To save desription",command=description)
+
+u =ListBox(app3,width=1000,height=500)
 
 app2.hide()
 app3.hide()
